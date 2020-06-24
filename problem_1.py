b7578ac1c3a93faa2c29eb23c4b51fc8dc196d89
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-24 14:53:15 +0800

    solve probelm 1

--- problem_1.py
+++ problem_1.py
@@ -1,14 +1,34 @@
 def sqrt(number):
-    """
-    Calculate the floored square root of a number
+   """
+   Calculate the floored square root of a number
 
-    Args:
-       number(int): Number to find the floored squared root
-    Returns:
-       int: Floored Square Root
-    """
-    pass
+   Args:
+      number(int): Number to find the floored squared root
+   Returns:
+      int: Floored Square Root
+   """
+   lower = 0
+   upper = number
+   answer = 0
+   # binary search -- require O(logn) time, n is the 
+   while lower <= upper:
+      mid = (lower + upper) // 2
+      squre = mid*mid
 
+      # first branch, search in the smaller part
+      if squre > number:
+         upper = mid - 1
+      # second branch, search in the larger part
+      if squre < number:
+         lower = mid + 1
+         answer = mid # update answer (since we want the floor of the sqrt)
+      # third branch
+      if squre == number:
+         return mid
+      
+   return answer
+
+# Test
 print ("Pass" if  (3 == sqrt(9)) else "Fail")
 print ("Pass" if  (0 == sqrt(0)) else "Fail")
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
