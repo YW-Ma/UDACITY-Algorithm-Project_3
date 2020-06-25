8b371f51aa3327fc9b38d288a0fbbc10ac7f4aca
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-25 13:32:30 +0800

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -5,7 +5,21 @@ def sort_012(input_list):
     Args:
        input_list(list): List to be sorted
     """
-    pass
+    # [list for 0] [list for 1] [list for 2]
+    list_0 = []
+    list_1 = []
+    list_2 = []
+
+    middle = 1
+    for i in input_list:
+        if i < middle:
+            list_0.append(i)
+        elif i > middle:
+            list_2.append(i)
+        else:
+            list_1.append(i)
+    return list_0 + list_1 + list_2
+
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
@@ -15,6 +29,14 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]) 
+# [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2]
+test_function([]) 
+# []
+test_function([1]) 
+# [1]
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+# [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
