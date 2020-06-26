683bb16a2b78996dfca6ca1d24e1c1633c5afe9c
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-26 16:08:52 +0800

    solve and explain problem 6

--- problem_6.py
+++ problem_6.py
@@ -1,16 +1,36 @@
+import sys
 def get_min_max(ints):
-    """
-    Return a tuple(min, max) out of list of unsorted integers.
+   """
+   Return a tuple(min, max) out of list of unsorted integers.
 
-    Args:
-       ints(list): list of integers containing one or more integers
-    """
-   pass
+   Args:
+      ints(list): list of integers containing one or more integers
+   """
+   if len(ints) == 0:
+      return None
+   max = -sys.maxsize
+   min = sys.maxsize
+   for i in ints:
+      if i <= min:
+         min = i
+      if i >= max:
+         max = i
+   return (min, max)
 
-## Example Test Case of Ten Integers
+
+## Test Cases
 import random
 
-l = [i for i in range(0, 10)]  # a list containing 0 - 9
+l = [i for i in range(0, 20)]  # a list containing 0 - 20
 random.shuffle(l)
+print ("Pass" if ((0, 19) == get_min_max(l)) else "Fail")
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+l = [i for i in range(-1500, 1000)]  # a list containing -1500 - 999
+random.shuffle(l)
+print ("Pass" if ((-1500, 999) == get_min_max(l)) else "Fail")
+
+l = [0]  # a list containing one element (0)
+print ("Pass" if ((0, 0) == get_min_max(l)) else "Fail")
+
+l = []  # a list containing nothing
+print ("Pass" if (None == get_min_max(l)) else "Fail")
